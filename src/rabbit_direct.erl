429f55ea10496091bbfdfefb89bc766c0e10dfe1
sky-big <15701683830@163.com>
2015-12-28 11:47:54 +0800

    RabbitMQ系统rabbit_direct类型连接的注释
    RabbitMQ系统rabbit_direct类型连接的注释

--- src/rabbit_direct.erl
+++ src/rabbit_direct.erl
@@ -15,6 +15,7 @@
 %%
 
 -module(rabbit_direct).
+%% 此模块是不通过TCP，而是直接调用RabbitMQ系统的接口启动rabbit_channel进程来发布或者消费消息等操作
 
 -export([boot/0, force_event_refresh/1, list/0, connect/5,
          start_channel/9, disconnect/2]).
@@ -60,36 +61,45 @@ force_event_refresh(Ref) ->
 	ok.
 
 
+%% 获取本节点启动的rabbit_drecit相关的进程
 list_local() ->
 	pg_local:get_members(rabbit_direct).
 
 
+%% 列出当前所有的direct类型的连接
 list() ->
 	rabbit_misc:append_rpc_all_nodes(rabbit_mnesia:cluster_nodes(running),
 									 rabbit_direct, list_local, []).
 
 %%----------------------------------------------------------------------------
-
+%% 开始连接的接口
 connect({none, _}, VHost, Protocol, Pid, Infos) ->
 	connect0(fun () -> {ok, rabbit_auth_backend_dummy:user()} end,
 			 VHost, Protocol, Pid, Infos);
 
 
+%% 开始连接的接口，只带有用户名
 connect({Username, none}, VHost, Protocol, Pid, Infos) ->
 	connect0(fun () -> rabbit_access_control:check_user_login(Username, []) end,
 			 VHost, Protocol, Pid, Infos);
 
 
+%% 开始连接的接口，带有用户名和密码
 connect({Username, Password}, VHost, Protocol, Pid, Infos) ->
+	%% 检查用户名和该用户的密码是否正确
 	connect0(fun () -> rabbit_access_control:check_user_pass_login(
 						 Username, Password) end,
 			 VHost, Protocol, Pid, Infos).
 
 
+%% 开始连接的接口
 connect0(AuthFun, VHost, Protocol, Pid, Infos) ->
+	%% 判断rabbit应用是否正在运行中
 	case rabbit:is_running() of
-		true  -> case AuthFun() of
+		true  -> %% 检查用户名和该用户的密码是否正确
+				 case AuthFun() of
 					 {ok, User = #user{username = Username}} ->
+						 %% 向rabbit_event事件中心发布direct类型的连接用户验证结果
 						 notify_auth_result(Username,
 											user_authentication_success, []),
 						 connect1(User, VHost, Protocol, Pid, Infos);
@@ -103,6 +113,7 @@ connect0(AuthFun, VHost, Protocol, Pid, Infos) ->
 	end.
 
 
+%% 向rabbit_event事件中心发布direct类型的连接用户验证结果
 notify_auth_result(Username, AuthResult, ExtraProps) ->
 	EventProps = [{connection_type, direct},
 				  {name, case Username of none -> ''; _ -> Username end}] ++
@@ -110,10 +121,14 @@ notify_auth_result(Username, AuthResult, ExtraProps) ->
 	rabbit_event:notify(AuthResult, [P || {_, V} = P <- EventProps, V =/= '']).
 
 
+%% 实际的连接操作
 connect1(User, VHost, Protocol, Pid, Infos) ->
+	%% 检查当前账号的客户端能否使用该VirtualHost
 	try rabbit_access_control:check_vhost_access(User, VHost, undefined) of
 		ok -> ok = pg_local:join(rabbit_direct, Pid),
+			  %% 向rabbit_event事件中心发布direct类型的连接连接成功
 			  rabbit_event:notify(connection_created, Infos),
+			  %% 返回用户名字和RabbitMQ系统的服务器特性
 			  {ok, {User, rabbit_reader:server_properties(Protocol)}}
 	catch
 		exit:#amqp_error{name = access_refused} ->
@@ -121,8 +136,11 @@ connect1(User, VHost, Protocol, Pid, Infos) ->
 	end.
 
 
+%% 启动direct类型连接下的rabbit_channel进程
 start_channel(Number, ClientChannelPid, ConnPid, ConnName, Protocol, User,
 			  VHost, Capabilities, Collector) ->
+	%% 在rabbit_direct_client_sup监督进程下启动rabbit_channel_sup监督进程，
+	%% 在rabbit_channel_sup监督进程下启动rabbit_channel和rabbit_limiter进程
 	{ok, _, {ChannelPid, _}} =
 		supervisor2:start_child(
 		  rabbit_direct_client_sup,
@@ -131,6 +149,7 @@ start_channel(Number, ClientChannelPid, ConnPid, ConnName, Protocol, User,
 	{ok, ChannelPid}.
 
 
+%% 关闭direct类型的连接
 disconnect(Pid, Infos) ->
 	pg_local:leave(rabbit_direct, Pid),
 	rabbit_event:notify(connection_closed, Infos).
